7456453ee19b889e581bdabc7c05eeae68628688
Krishnanand <krisnanandb@gmail.com>
2016-06-18 15:43:27 +0530

    FirstCommit

---
+++ src/main/java/com/dreamersweekend/nj6/Nj6NiraamayaApplication.java
@@ -0,0 +1,12 @@
+package com.dreamersweekend.nj6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nj6NiraamayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nj6NiraamayaApplication.class, args);
+	}
+}
