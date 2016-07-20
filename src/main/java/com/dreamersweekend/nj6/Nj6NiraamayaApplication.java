96c6962ab2951bf6c543ea910b9b49a9b5925954
Krishnanand B <kbandame@gmail.com>
2016-07-20 23:24:43 +0530

    Run now

--- src/main/java/com/dreamersweekend/nj6/Nj6NiraamayaApplication.java
+++ src/main/java/com/dreamersweekend/nj6/Nj6NiraamayaApplication.java
@@ -8,5 +8,6 @@ public class Nj6NiraamayaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Nj6NiraamayaApplication.class, args);
+		System.out.println("Made a change run now");
 	}
 }
